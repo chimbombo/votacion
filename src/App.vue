71c453324aba4f2ed86f665c999a9af1eb8fcc16
NoNo <antonio.galaz.aroca>
2020-07-10 19:01:41 -0400

    html del register listo

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Login</router-link> |
-      <router-link to="/register">Register</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
-<style scoped>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
-  background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,214 +1,68 @@
 <template>
-    <div>
-<form>
-  <div class="head-form">
-    <h1>Sign Up</h1>
-  </div>
-  <div class="con">
-    <input class="form-input" type="text" placeholder="Email" />
-    <input class="form-input" type="email" placeholder="Email Address" />
-    <input class="form-input" type="password" placeholder="Password" />
-    <input class="form-input" type="password" placeholder="Repeat Password" />
-    <button class="btn">Clear</button>
-    <button class="btn">Register</button>
-  </div>
-</form>  
+  <div>
+    <div class="container">
+      <div>
+        <h1 align="center">Register form</h1>
+      </div>
+      <form>
+        <div class="form-group">
+          <label for="inputName">Name</label>
+          <input type="text" class="form-control" id="inputName" v-model="inputName"/>
+        </div>
+        <div class="form-group">
+          <label for="inputDate">Birth date</label>
+          <input type="date" class="form-control" name="inputDate" v-model="inputDate" />
+        </div>
+        <div class="form-group">
+          <label for="inputEmail">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="inputEmail"
+            aria-describedby="emailHelp"
+            placeholder="otaku@otaku.com"
+            v-model="inputEmail"
+          />
+          <small
+            id="emailHelp"
+            class="form-text text-muted"
+          >We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword1">Password</label>
+          <input type="password" class="form-control" id="inputPassword1" v-model="inputPassword1"/>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword2">Repeat Password</label>
+          <input type="password" class="form-control" id="inputPassword1" v-model="inputPassword2"/>
+        </div>
+        <button type="submit" :disabled='!passwordDisabled' class="btn btn-primary">Register</button>
+      </form>
     </div>
+  </div>
 </template>
 
+<script>
+import {mapActions, mapState} from 'vuex'
 
-<style scoped>
-/* Fonts Form Google Font ::- https://fonts.google.com/  -:: */
-@import url('https://fonts.googleapis.com/css?family=Abel|Abril+Fatface|Alegreya|Arima+Madurai|Dancing+Script|Dosis|Merriweather|Oleo+Script|Overlock|PT+Serif|Pacifico|Playball|Playfair+Display|Share|Unica+One|Vibur');
-/* End Fonts */
-/* Start Global rules */
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-/* End Global rules */
-
-/* Start body rules */
-body {
-    background-image: linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%);
-background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
-background-attachment: fixed;
-  background-repeat: no-repeat;
-
-    font-family: 'Vibur', cursive;
-/*   the main font */
-    font-family: 'Abel', sans-serif;
-opacity: .95;
-/* background-image: linear-gradient(to top, #d9afd9 0%, #97d9e1 100%); */
-}
-
-
-
-/* |||||||||||||||||||||||||||||||||||||||||||||*/
-/* //////////////////////////////////////////// */
-
-
-
-
-/* End body rules */
-
-/* Start form  attributes */
-form {
-    width: 450px;
-    min-height: 500px;
-    height: auto;
-    border-radius: 5px;
-    margin: 2% auto;
-    box-shadow: 0 9px 50px hsla(20, 67%, 75%, 0.31);
-    padding: 2%;
-    background-image: linear-gradient(-225deg, #E3FDF5 50%, #FFE6FA 50%);
-}
-/* form Container */
-form .con {
-    display: -webkit-flex;
-    display: flex;
-  
-    -webkit-justify-content: space-around;
-    justify-content: space-around;
-  
-    -webkit-flex-wrap: wrap;
-    flex-wrap: wrap;
-  
-      margin: 0 auto;
-}
-
-/* the header form form */
-header {
-    margin: 2% auto 10% auto;
-    text-align: center;
-}
-/* Login title form form */
-header h2 {
-    font-size: 250%;
-    font-family: 'Playfair Display', serif;
-    color: #3e403f;
-}
-/*  A welcome message or an explanation of the login form */
-header p {letter-spacing: 0.05em;}
-
-
-
-/* //////////////////////////////////////////// */
-/* //////////////////////////////////////////// */
-
-
-.input-item {
-    background: #fff;
-    color: #333;
-    padding: 14.5px 0px 15px 9px;
-    border-radius: 5px 0px 0px 5px;
-}
-
-
-
-/* Show/hide password Font Icon */
-#eye {
-    background: #fff;
-    color: #333;
-  
-    margin: 5.9px 0 0 0;
-    margin-left: -20px;
-    padding: 15px 9px 19px 0px;
-    border-radius: 0px 5px 5px 0px;
-  
-    float: right;
-    position: relative;
-    right: 1%;
-    top: -.2%;
-    z-index: 5;
-    
-    cursor: pointer;
-}
-/* inputs form  */
-input[class="form-input"]{
-    width: 240px;
-    height: 50px;
-  
-    margin-top: 2%;
-    padding: 15px;
-    
-    font-size: 16px;
-    font-family: 'Abel', sans-serif;
-    color: #5E6472;
-  
-    outline: none;
-    border: none;
-  
-    border-radius: 0px 5px 5px 0px;
-    transition: 0.2s linear;
-    
-}
-input[id="txt-input"] {width: 250px;}
-/* focus  */
-input:focus {
-    transform: translateX(-2px);
-    border-radius: 5px;
-}
-
-/* //////////////////////////////////////////// */
-/* //////////////////////////////////////////// */
-
-/* input[type="text"] {min-width: 250px;} */
-/* buttons  */
-button {
-    display: inline-block;
-    color: #252537;
-  
-    width: 280px;
-    height: 50px;
-  
-    padding: 0 20px;
-    background: #fff;
-    border-radius: 5px;
-    
-    outline: none;
-    border: none;
-  
-    cursor: pointer;
-    text-align: center;
-    transition: all 0.2s linear;
-    
-    margin: 7% auto;
-    letter-spacing: 0.05em;
-}
-/* Submits */
-.submits {
-    width: 48%;
-    display: inline-block;
-    float: left;
-    margin-left: 2%;
-}
-
-/*       Forgot Password button FAF3DD  */
-.frgt-pass {background: transparent;}
-
-/*     Sign Up button  */
-.sign-up {background: #B8F2E6;}
-
-
-/* buttons hover */
-button:hover {
-    transform: translatey(3px);
-    box-shadow: none;
-}
-
-/* buttons hover Animation */
-button:hover {
-    animation: ani9 0.4s ease-in-out infinite alternate;
-}
-@keyframes ani9 {
-    0% {
-        transform: translateY(3px);
+export default {
+  name:'Register',
+  data(){
+    return{
+      inputName: '',
+      inputDate:'',
+      inputEmail: '',
+      inputPassword1: '',
+      inputPassword2: ''
     }
-    100% {
-        transform: translateY(5px);
-    }
-}
+  },
+  methods:{
 
-</style>
+  },
+  computed:{
+    passwordDisabled(){
+      return this.inputPassword1 === this.inputPassword2 && this.inputPassword1.trim() !== '' && this.inputPassword1.length > 5
+    }
+  }
+}
+</script>
